f6a49687c5ad11aa14469f59902a314740d55c5c
weston <westondease@gmail.com>
2019-02-07 20:52:17 -0500

    WE MADE IT BOYS

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -43,18 +43,42 @@ module.exports = function (app) {
   });
 
   app.get('/api/nightmare', function(req, res) {
+    let message = '--- \n\
+    title: Nightmare \n\
+    published: true \n\
+    description: This is an automated post using nightmare.js \n\
+    tags: nightmare.js \n\
+    ---\n\
+    This is an automated post made entirely using nightmare!';
     console.log("We are in our api routes handling a nightmare request");
     nightmare
-    .goto('https://dev.to')
-    .wait(2000)
+    .goto('https://dev.to/users/auth/github?state=join-club-page_basic')
+    .type('#login_field', '')
+    .type('#password', '')
+    .click('.btn-primary')
+    // .wait(20000)
+    // .evaluate( () => {
+    //   console.log('here it is' , document.body.innerHTML);
+    // })
+    .wait(20000)
+    .goto('https://dev.to/new')
+    // .evaluate(() => {
+    //  return document.title
+    // })
+    .evaluate(function() {
+      document.querySelector('#article_body_markdown').value = ''
+      })
+    .insert('#article_body_markdown', message)
+    .click('#article-submit')
+    // .wait(10000)
     //.type('#search_form_input_homepage', 'github nightmare')
     //.click('#search_button_homepage')
     //.wait('#r1-0 a.result__a')
     //.evaluate(() => document.querySelector('#r1-0 a.result__a').href)
     //.end()
-    .then(console.log)
+    .then((title) => console.log(title))
     .catch(error => {
-      console.error('Search failed:', error)
+     console.error('Search failed:', error)
     })
   });
 }
